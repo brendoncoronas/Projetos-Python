bb9df8fd17c229ef18791eccecdeb330171fcf75
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-06 13:18:12 -0300

    Add files via upload
    projetei uma calculadora um pouco mais avançada, tentei cobrir o maximo de bugs.

---
+++ Projeto7_EX_calculadora_Avancada.py
@@ -0,0 +1,60 @@
+""" calculadora com whhile """
+
+# while True:  
+#     sair = input('deseja sair? [s]im:')
+#     sair = sair.lower() # retorna a mesma str enviada em "sair" (input) em letras minusculas.
+#     sair = sair.startswith('s')  # estamos checando se a palavra que for digitada começa com 's'
+
+# outra forma de fazer isso mais pratico:
+
+while True:
+    num_1 =  input('digite um numero:')
+    num_2 =  input('digite outro numero:')
+    operador =  input('digite o operador (-+/*):')
+    
+    numeros_validos = None
+
+    num_1_float = 0 # estamos definindo as 2 variaveis fora do laço,boa pratica de conduta
+    num_2_float = 0
+
+    try:
+        num_1_float = float(num_1)
+        num_2_float = float(num_2)
+        numeros_validos = True  # estamos usando essa VAR para checagem, se n gerar erro no try logo ela se torna True
+
+    except:    
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('um ou ambos os numeros digitados são invalidos.') 
+        continue   
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('operador invalidos.') 
+        continue 
+
+    if len(operador) > 1:  # se digitar mais q um operador
+        print('digite apenas um operador.') 
+        continue 
+    
+    print('realizando sua conta. Confira o resultado abaixo:')
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} =', num_1_float + num_2_float)
+
+    if operador == '-':
+        print(f'{num_1_float} - {num_2_float} =',num_1_float - num_2_float)
+
+    if operador == '/':
+        print(f'{num_1_float} / {num_2_float} =',num_1_float / num_2_float)
+
+    if operador == '*':
+        print(f'{num_1_float} * {num_2_float} =',num_1_float * num_2_float)
+
+
+    sair = input('deseja sair? [s]im:').lower().startswith('s') # startswith esta atrelado ao q esta sendo retornado pelo Lower,e o lower estar sendo atrelado ao input
+
+    if sair is True:
+        print('voce saiu da calculadora.')
+        break
