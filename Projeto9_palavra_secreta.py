dc5f0632f37e7d7b5903b28d6a3b5b41dd055e02
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-08 19:42:17 -0300

    Add files via upload
    projeto de um jogo da forca.

---
+++ Projeto9_palavra_secreta.py
@@ -0,0 +1,47 @@
+
+palavra_secreta = 
+letra_acertadas = 
+numeros_tentativas = 
+
+
+while True:
+    letra_digitada = input('digite uma letra:')
+    numeros_tentativas += 1
+
+    if len(letra_digitada) > 1:
+        print('figite apenas uma letra.')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letra_acertadas += letra_digitada
+
+    palavra_formada = ''
+
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letra_acertadas:
+            palavra_formada += letra_secreta
+
+        else:
+            palavra_formada += '*'
+
+    print('palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system('cls')
+
+        print('VOCÊ GANHOU!! PARABÊNS!')
+        print('a palavra era', palavra_secreta)     
+        print('Tentativas:', numeros_tentativas)   
+        
+        letra_acertadas = ''
+        numeros_tentativas = 0
+
+
+
+
+
+
+
+
+
+
