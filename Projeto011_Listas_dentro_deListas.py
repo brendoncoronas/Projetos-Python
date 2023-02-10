50b9c64ac39a2479685cfe360757210e51f67d91
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-10 20:23:13 -0300

    Add files via upload

---
+++ Projeto011_Listas_dentro_deListas.py
@@ -0,0 +1,19 @@
+
+#podemos usar FOR
+
+salas = [
+    
+    ['maria', 'helena',], # 0
+    
+    ['elaina',], # 1
+         1      2
+    ['luiz', 'joao', 'eduardo',], # 
+
+]
+print(salas[2][3][3])
+
+
+for sala in salas: # para cada sala na variavel SALAS, retorne a sala
+    print(f'a sala é {sala}')
+    for aluno in sala:  #nota-se que sala é no singular # para cada aluna na SALA retorne o aluno
+        print(aluno)
