9c5e692e5844e71f51ce6269fd330a792759370c
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-12 12:01:16 -0300

    Add files via upload

---
+++ Projeto013_Gerando100_CPFs.py
@@ -0,0 +1,31 @@
+import random # gera coisas aleatorias
+
+for _ in range(100):
+    nove_digitos = ''
+
+    for i in range(9):  
+        nove_digitos += random.randint(0, 9)  # .randint é para gerar numeros aleatirios entre o que qremos (entre 0 e 9 no caso) ja diz o nome randINT
+
+    contador_regressivo_1 = 10
+    resultado_1 = 0 
+    for digito in nove_digitos  :
+        resultado_1 += int(digito) * contador_regressivo_1
+        contador_regressivo_1 -= 1  # cada volta do laço diminui 1
+
+    digito_1 = (resultado_1 * 10) % 11
+
+    digito_1 = digito_1 if digito_1 <= 9 else 0  # digito vai ser igual a digito se digito for menor ou igual a 9, senão retorna 0
+
+
+    dez_digitos = nove_digitos + str(digito_1)
+    contador_regressivo_2 = 11
+
+    resultado_digito_2 = 0
+    for digito in dez_digitos:
+        resultado_digito_2 += int(digito) * contador_regressivo_2
+        contador_regressivo_2 -= 1
+
+    digito_2 = (resultado_digito_2 * 10) % 11
+    digito_2 = digito_2 if digito_2 <= 9 else 0
+
+    cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
