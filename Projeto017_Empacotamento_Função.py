c7fdf7d871fbba40d97cf1d33c4a2a3506eab31b
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-16 19:45:45 -0300

    Add files via upload

---
+++ Projeto017_Empacotamento_Função.py
@@ -0,0 +1,15 @@
+"""
+crie uma função que multiplica todos os argumentos
+não nomeados recebidos
+retorne o total para uma variavel e mostre o valor
+da variavel.
+"""
+
+def multiplicar(*args): 
+    total = 1 
+    for numero in args:
+        total *= numero
+    return total
+multiplicacao = multiplicar(1, 2, 3, 4, 5) 
+print(multiplicacao)
+
