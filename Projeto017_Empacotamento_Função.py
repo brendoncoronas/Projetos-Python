d7b702d56da93344a0432d574d152a56adc7ad0c
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-16 19:46:20 -0300

    Update Projeto017_Empacotamento_Função.py

--- Projeto017_Empacotamento_Função.py
+++ Projeto017_Empacotamento_Função.py
@@ -1,5 +1,5 @@
 """
-cria uma função que multiplica todos os argumentos
+criar uma função que multiplica todos os argumentos
 não nomeados recebidos
 retorne o total para uma variavel e mostre o valor
 da variavel.
