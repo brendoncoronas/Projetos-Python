94f61cc8ee716ac717df31e8d73c924622d12015
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-17 20:03:06 -0300

    Update aula60_EX_Função.py

--- aula60_EX_Função.py
+++ aula60_EX_Função.py
@@ -1,24 +1,8 @@
-"""
-crie uma função que multiplica todos os argumentos
-não nomeados recebidos
-retorne o total para uma variavel e mostre o valor
-da variavel.
-"""
-
-def multiplicar(*args): 
-    total = 1 # aqui como é multiplicação começamos do 1 pois qualquer numero vezes 0 é zero
-    for numero in args:
-        total *= numero
-    return total
-multiplicacao = multiplicar(1, 2, 3, 4, 5) # aqui passamos os numeros que vão ser enpacotados, para ai sim fazermos a conta no DEF
-print(multiplicacao)
-
 
 """
 criar uma função se o numero é par ou impar
 retorne se o numero é par ou impar
 """
-
 
 
 def par_impar(numero):
