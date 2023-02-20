625d06f442c6e66f616323046246e38fd09a5d72
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-20 13:00:29 -0300

    Update Projeto020_ListaComVariasOpções.py

--- Projeto020_ListaComVariasOpções.py
+++ Projeto020_ListaComVariasOpções.py
@@ -3,7 +3,7 @@ def do_redo(todo_list,redo_list):
     if not redo_list:
         print('nada para refazer.'
         return
-    last_redo = redo_list.pop(
+    last_redo = redo_list.pop()
     todo_list.append(last_redo
 
 def show_op(todo_list:
