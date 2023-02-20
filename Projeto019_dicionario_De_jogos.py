d9c15517d99d796c28642cea44a8c0c0dadb6cf2
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-19 22:21:12 -0300

    Add files via upload

---
+++ Projeto019_dicionario_De_jogos.py
@@ -0,0 +1,13 @@
+
+
+games = {'nome':[], 'videogame':[],'ano':[]}
+for i in range (3):
+        nome = input('qual o nome do jogo?'
+        videogame = input('qual o videogame?'
+        ano = input('qual o ano do lançamento?'
+        games['nome'].append(nome)
+        games['videogame'].append(videogame)
+        games['ano'].append(ano
+print('-' * 20)
+print(games)
+
