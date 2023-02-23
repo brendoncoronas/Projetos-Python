186cb2f2f2d86e0056f8844d3cf749b5d160f649
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-23 20:11:53 -0300

    Update Projeto023_comSET_primeiroDuplicado_considerandoSegunda_Ocorrencia.py

--- Projeto023_comSET_primeiroDuplicado_considerandoSegunda_Ocorrencia.py
+++ Projeto023_comSET_primeiroDuplicado_considerandoSegunda_Ocorrencia.py
@@ -27,7 +27,7 @@ lista_de_lista_de_inteiros = [
     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1,],
 ]
 
-def encontra_primeiro_duplicado(lista_de_inteiros
+def encontra_primeiro_duplicado(lista_de_inteiros)
     numeros_checados = set()
     primeiro_duplicado = -1  # como padrão ja iniciaremos com o -1
 
