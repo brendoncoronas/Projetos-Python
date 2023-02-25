908042860d16cb4e7da6c9d3dc99477b6111e96b
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-25 20:39:50 -0300

    Add files via upload

---
+++ Projeto025_yieldFrom.py
@@ -0,0 +1,36 @@
+# yield from 
+
+# é exelente para fazer continuações 
+
+def gen1():
+    print('começou GEN1'
+    yield 1
+    yield 2
+    yield 3
+    print('acabou GEN1'
+
+def gen3():
+    print('começou GEN3'
+    yield 10
+    yield 20
+    yield 30
+    print('terminou GEN3'
+
+
+def gen2(gen):
+    print('começou GEN2')
+    yield from gen()  
+    yield 4
+    yield 5
+    yield 6
+    print('acabou GEN2')
+
+
+g1 = gen2(gen1)
+g2 = gen2(gen3)
+
+for numero in g1:
+    print(numero)
+
+for numero in g2:
+    print(numero)
