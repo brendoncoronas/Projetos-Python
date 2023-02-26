c5350a1f8211367e110721085a5163f5181437f2
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-26 15:07:42 -0300

    Add files via upload

---
+++ Projeto026_getter_setter.py
@@ -0,0 +1,16 @@
+
+class Pessoa:
+    def __init__(self,nome:
+        self._nome = nome
+
+    @property  # getter ta retornando o valor
+    def nome(self:
+        return self._nome
+
+    @nome.setter  
+    def nome(self, nome: 
+        self.atributo = nome  
+
+p1 = Pessoa()
+p1.nome = 'joao'
+print(p1.nome
