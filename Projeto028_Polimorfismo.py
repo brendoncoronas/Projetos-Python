53a103a9ad53bafd2be25a038473afe1d23623ff
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-28 19:57:46 -0300

    Add files via upload

---
+++ Projeto028_Polimorfismo.py
@@ -0,0 +1,22 @@
+
+from abc import ABC, abstractmethod
+
+class A(ABC):
+    @abstractmethod   
+    def fala(self, msg: pass
+
+class B(A):
+    def falar(self, msg:
+        print(f'B esta falando {msg}'
+
+
+class C(A):
+    def falar(self, msg):
+        print(f'C esta falando {msg}'
+
+
+
+b = B()
+c = C()
+b.fala('um assunto')
+c.fala('outro assunto')
