7e56d040eda43ce27ed5bb72927b69f529d1a5b0
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-02-27 23:10:53 -0300

    Add files via upload

---
+++ Projeto027_RAISE_lançando_exceções.py
@@ -0,0 +1,28 @@
+# raise - lançando exceções (erros)
+
+def nao_aceito_zero(d:
+    if d == 0:
+        raise ZeroDivisionError('voce esta tentando dividir por zero.'
+    
+    return True
+
+def deve_ser_int_ou_float(n: 
+    tipo_n = type(n 
+    
+    if not isinstance(n, (float, int)):
+        raise TypeError(
+            f'{n} deve ser int ou float.'
+            f'{tipo_n.__name__}'
+        )
+
+
+def divide(n, d):
+    deve_ser_int_ou_float(n)
+    deve_ser_int_ou_float(d)
+    nao_aceito_zero(d)
+    return n / d
+
+
+print(divide(8/0))
+# com raise podemos levantar nossas proprias exceçoes.
+
