752e629c0c90c0a928cd70016a45b6e74ce3501b
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-01 19:29:16 -0300

    Update Projeto029_base_de_dados.py

--- Projeto029_base_de_dados.py
+++ Projeto029_base_de_dados.py
@@ -1,6 +1,6 @@
 class BaseDeDados:
     def __init__(self):
-        self.__dados = {  
+        self.__dados = {}  
     def inserir_cliente(self, id, nome:
         if 'clientes' not in self.__dados:
             self.__dados['clientes'] = {id: nome
