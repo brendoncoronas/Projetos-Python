76ced34539633d566cc80359cd6558eef6bd0ee3
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-04 19:14:20 -0300

    Add files via upload

---
+++ Projeto032_ordenando_do_menor_para_maior.py
@@ -0,0 +1,26 @@
+
+# ordene os produtos por nome crecente (do menor para maior)
+# gere produtos_ordenados_por_preco por deep copy (copia profunda)
+import copy
+
+produtos = [
+    {'nome': 'produto 5', 'preco': 10.00},
+    {'nome': 'produto 1', 'preco': 22.32},
+    {'nome': 'produto 3', 'preco': 10.11},
+    {'nome': 'produto 2', 'preco': 105.87},
+    {'nome': 'produto 4', 'preco': 69.90},
+
+
+
+produtos_ordenados_por_preco = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['preco'],
+    
+)
+
+print(*produtos, sep='\n'
+print()
+print(*novos_produtos, sep='\n'
+print(*produtos_ordenados_por_nome, sep='\n')
+print()
+print(*produtos_ordenados_por_preco, sep='\n') 
