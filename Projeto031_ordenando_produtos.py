f964060415e694a07ba87ba5daf5bd3bff35c107
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-03 21:23:34 -0300

    Update Projeto031_ordenando_produtos.py

--- Projeto031_ordenando_produtos.py
+++ Projeto031_ordenando_produtos.py
@@ -5,7 +5,7 @@
 import copy
 
 produtos = [
-    {'nome': 'produto 5', 'preco': 10.00,
+    {'nome': 'produto 5', 'preco': 10.00},
     {'nome': 'produto 1', 'preco': 22.32,
     {'nome': 'produto 3', 'preco': 10.11},
     {'nome': 'produto 2', 'preco': 105.87},
