8280ac4bd8ea91643016e7d936f3819ba86dd662
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-05 20:08:01 -0300

    Update Projeto033_adiando_execuções.py

--- Projeto033_adiando_execuções.py
+++ Projeto033_adiando_execuções.py
@@ -5,7 +5,7 @@ closure e funções que retornam outras funções
 
 """
 
-def  criar_saudacao(saudacao:
+def  criar_saudacao(saudacao):
     def saudar(nome:
         return f'{saudacao}, {nome}!'
     return saudar 
