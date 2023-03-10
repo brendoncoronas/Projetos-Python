f2d8d2e42d781628271f92a24fe89ce273f1c813
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-10 10:10:39 -0300

    Add files via upload

---
+++ Projeto038_ordenar_grupos.py
@@ -0,0 +1,26 @@
+# criar uma funcao que ordene uma lista com dicionarios
+# faça da maneira mais eficiente que conseguir 
+
+from  itertools import groupby
+
+alunos = [
+    {'nome': 'luiz', 'nota': 'A'},
+    {'nome': 'leticia', 'nota': 'B'},
+    {'nome': 'fabricio', 'nota': 'A'},
+    {'nome': 'rosemary', 'nota': 'C'},
+    {'nome': 'joana', 'nota': 'D'},
+    {'nome': 'joao', 'nota': 'A'},
+
+
+
+def ordena(aluno:   # estamos ordenando por nota
+    return aluno['nota'
+
+alunos_agrupados = sorted(alunos, key=ordena
+
+grupos = groupby(alunos_agrupados, key=ordena
+
+for chave, grupo in grupos:
+    print(chave)
+    for aluno in grupos:
+        print(aluno)
