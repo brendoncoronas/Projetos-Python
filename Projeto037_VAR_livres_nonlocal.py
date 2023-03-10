e3df5b4929c236bafac1179b6c642717b6e5b97f
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-09 22:34:58 -0300

    Add files via upload

---
+++ Projeto037_VAR_livres_nonlocal.py
@@ -0,0 +1,19 @@
+
+# crie uma função que contenha variavel livre
+# temos que alterar o valor dela concatenando com algo 
+
+def concatenar(string_inicial:
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar:
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+c = concatenar('a'
+
+print(c('b'))
+print(c('c'))
+
+
