cbdf870ea9adaaed9e66a59cd9fc59c3cfdb5b0e
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-09 22:35:53 -0300

    Update Projeto037_VAR_livres_nonlocal.py

--- Projeto037_VAR_livres_nonlocal.py
+++ Projeto037_VAR_livres_nonlocal.py
@@ -5,7 +5,7 @@
 def concatenar(string_inicial):
     valor_final = string_inicial
 
-    def interna(valor_a_concatenar:
+    def interna(valor_a_concatenar):
         nonlocal valor_final
         valor_final += valor_a_concatenar
         return valor_final
