d61f3c11646230ddd8817a995607bab5c5896d70
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-19 08:45:54 -0300

    Add files via upload

---
+++ Projeto046_endereços_COMPOSIÇÃO.py
@@ -0,0 +1,33 @@
+# faça um projeto com 
+# uso CLARO de composição
+# de no minimo dois metodos 
+
+
+class Cliente:
+    def __init__(self, nome:
+        self.nome = nome 
+        self.endereco = [
+
+    def inserir_endereco(self, rua, numero:
+        self.endereco.append(Endereco(rua, numero)
+
+    def listar_enderecos(self):
+        for endereco in self.endereco:
+            print(endereco.rua, endereco.numero)    
+
+    def __del__(self):
+        print('APAGANDO,', self.nome)   
+
+
+
+class Endereco:
+    def __init__(self, rua, numero):
+        self.rua = rua
+        self.numero = numero 
+
+    def __del__(self):
+        print('APAGANDO,', self.rua, self.numero)    
+
+cliente1 = Cliente('maria')
+cliente1.inserir_endereco('Av Brasil', 54)
+cliente1.inserir_endereco('rua B', 6745)
