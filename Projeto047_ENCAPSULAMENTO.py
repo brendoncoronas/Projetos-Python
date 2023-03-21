20a1ace411aad9ddb289887276172a26c5258832
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-20 22:39:42 -0300

    Add files via upload

---
+++ Projeto047_ENCAPSULAMENTO.py
@@ -0,0 +1,26 @@
+# crie um projeto com todas 
+# que mostre todas as usabilidades
+# do encapsulamento, na mesma classes
+
+
+class Foo:
+    def __init__(self:
+        self.public = 'isso é publico'
+        self._protected = 'isso é protegido'
+        self.__private = 'isso é privado'
+
+    def metodo_publico(self:
+        print(self.__private
+        return 'metodo_publico'
+
+    def _metodo_protegido(self:
+        return '_metodo_protegido'
+    
+    def __metodo_private(self:
+        return '__metodo_privado'
+    
+    
+f = Foo()
+print(f.metodo_publico)
+print(f._metodo_protegido)
+print(f.__metodo_private)
