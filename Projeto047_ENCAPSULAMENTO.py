54f3485a352a1cba2594a3cd8f7ddad0610c7188
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-03-20 22:40:08 -0300

    Update Projeto047_ENCAPSULAMENTO.py

--- Projeto047_ENCAPSULAMENTO.py
+++ Projeto047_ENCAPSULAMENTO.py
@@ -4,7 +4,7 @@
 
 
 class Foo:
-    def __init__(self:
+    def __init__(self):
         self.public = 'isso é publico'
         self._protected = 'isso é protegido'
         self.__private = 'isso é privado'
