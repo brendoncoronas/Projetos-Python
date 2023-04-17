5394427a460e51d3f61cd675002b1bfccc908174
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-17 09:35:43 -0300

    Update Projeto050_CLASSE_DECORADORA.py

--- Projeto050_CLASSE_DECORADORA.py
+++ Projeto050_CLASSE_DECORADORA.py
@@ -1,6 +1,6 @@
 # classes decoradora
 # faça um projeto que mostre o uso
-# claro de uma classe decorador
+# claro de uma classe decoradora
 
 class Multiplicador:
     def __init__(self, multiplicador):
