b0abcb5b2f13999cd90734629abeb6c6d36bd632
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-19 19:54:45 -0300

    Add files via upload

---
+++ Projeto051_enum.Enum.py
@@ -0,0 +1,29 @@
+import enum  # 1         #2
+
+# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
+
+
+class Direcoes(enum.Enum:
+    ESQUERDA = enum.auto( # enumera automaticamente
+    DIREITA = enum.auto(
+    ACIMA = enum.auto(
+    ABAIXO = enum.auto(
+
+
+print(Direcoes(1), Direcoes['ESQUERDA'], Direcoes.ESQUERDA)
+
+# estamos imformando que direcao é do tipo de Direcoes
+
+
+def mover(direcao: Direcoes):
+    if not isinstance(direcao, Direcoes):
+        raise ValueError('direção não encontrada')
+
+    # {direcao.name} é so para aparecer o nome quando dermos print, sem isso aparece mais coisa alem do nome
+    print(f'movendo para {direcao.name} ({direcao.value})')
+
+
+mover(Direcoes.ESQUERDA)
+mover(Direcoes.DIREITA)
+mover(Direcoes.ACIMA)
+mover(Direcoes.ABAIXO)
