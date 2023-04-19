4f3e94e3bc15b4b726b3e58c2f74809dc8c0742c
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-19 19:55:05 -0300

    Update Projeto051_enum.Enum.py

--- Projeto051_enum.Enum.py
+++ Projeto051_enum.Enum.py
@@ -4,7 +4,7 @@ import enum  # 1         #2
 
 
 class Direcoes(enum.Enum:
-    ESQUERDA = enum.auto( # enumera automaticamente
+    ESQUERDA = enum.auto() # enumera automaticamente
     DIREITA = enum.auto(
     ACIMA = enum.auto(
     ABAIXO = enum.auto(
