63a89b8c086f4cef2a5ab0a77470b98ed4d71426
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-20 09:57:30 -0300

    Add files via upload

---
+++ Projeto052_CALL.py
@@ -0,0 +1,11 @@
+class CallMe:
+    def __init__(self, phone:
+        self.phone = phone
+
+    def __call__(self, nome:
+        print(nome, 'chamando', self.phone
+
+
+call1 = CallMe('3857617618' # é para ser um numero de celular
+call1('brendon coronas'  # sabemos que normalmente isso nao é executavel
+# porem com o __call__ torna viavel a execução
