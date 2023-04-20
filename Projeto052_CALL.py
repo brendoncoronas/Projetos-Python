94a1e7a145d210a241bd30640e7195716528ea38
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-20 09:58:30 -0300

    Update Projeto052_CALL.py

--- Projeto052_CALL.py
+++ Projeto052_CALL.py
@@ -7,5 +7,5 @@ class CallMe:
 
 
 call1 = CallMe('3857617618') # é para ser um numero de celular
-call1('brendon coronas'  # sabemos que normalmente isso nao é executavel
+call1('brendon coronas')  # sabemos que normalmente isso nao é executavel
 # porem com o __call__ torna viavel a execução
