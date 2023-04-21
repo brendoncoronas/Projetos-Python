950a9f87ff3c655b442c94a4e664834b77ea223f
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-21 14:48:27 -0300

    Add files via upload

---
+++ Projeto053_GROUPBY.py
@@ -0,0 +1,23 @@
+from  itertools import groupby
+
+alunos = [
+    {'nome': 'luiz', 'nota': 'A'
+    {'nome': 'leticia', 'nota': 'B'
+    {'nome': 'fabricio', 'nota': 'A'
+    {'nome': 'rosemary', 'nota': 'C'
+    {'nome': 'joana', 'nota': 'D'
+    {'nome': 'joao', 'nota': 'A'
+]
+
+
+def ordena(aluno:   # estamos ordenando por nota
+    return aluno['nota'
+
+alunos_agrupados = sorted(alunos, key=ordena
+
+grupos = groupby(alunos_agrupados, key=ordena
+
+for chave, grupo in grupos:
+    print(chave
+    for aluno in grupos:
+        print(aluno
