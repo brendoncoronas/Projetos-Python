10985328a747fc9d6e38541e727d7519b7615288
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-22 21:24:09 -0300

    Add files via upload

---
+++ Projeto054_SUPER.py
@@ -0,0 +1,25 @@
+
+class A
+    atributo_a = 'valor a
+
+    def metodo(self
+        print('A'
+
+class B(A
+    atributo_b = 'valor b
+
+    def metodo(self
+        print('B'
+
+class C(B
+    atributo_c = 'valor c
+
+    def metodo(self
+        super(B, self).metodo(
+        print('C
+
+c = C()
+print(c.atributo_a
+print(c.atributo_b
+print(c.atributo_c
+c.metodo(
