431065f6c16bad22c123b951cead9ef95008ec2f
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-23 21:48:30 -0300

    Update Projeto055_funcoes_decoradora_eDecoradores_com_classes.py

--- Projeto055_funcoes_decoradora_eDecoradores_com_classes.py
+++ Projeto055_funcoes_decoradora_eDecoradores_com_classes.py
@@ -1,7 +1,7 @@
 
 
 def adiciona_repr(cls):
-    def meu_repr(self
+    def meu_repr(self)
         class_name = self.__class__.__name__
         class_dict = self.__dict__
         class_repr = f'{class_name({class_dict'
