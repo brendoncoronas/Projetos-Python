4d596fb260ab8e67425a07db105dd54ec6e4a168
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-25 09:39:00 -0300

    Add files via upload

---
+++ Projeto057_MetaClasse.py
@@ -0,0 +1,20 @@
+class Meta(type
+    def __new__(mcs, name, bases, dct
+        print('metaclass new
+        cls = super().__new__(mcs, name, bases, dct
+        cls.attr = 1234
+        return cls
+
+
+class Pessoa(metaclass=Meta
+    def __new__(cls, *args, **kwargs 
+        print('meu new
+        instancia = super().__new__(cls
+        return instancia
+    
+    def __init__(self, nome
+        print('meu init
+        self.nome = nome
+
+p1 = Pessoa('luiz 
+print(p1.attr
