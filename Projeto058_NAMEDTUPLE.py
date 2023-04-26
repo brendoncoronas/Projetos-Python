dd71427fe74493cdcdfe089eb8a1a0801d148328
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-26 09:30:48 -0300

    Update Projeto058_NAMEDTUPLE.py

--- Projeto058_NAMEDTUPLE.py
+++ Projeto058_NAMEDTUPLE.py
@@ -6,6 +6,6 @@ Carta = namedtuple('carta', ['valor', 'naipe']) # type: ignore
 
 as_espadas = Carta('A', 'espada')
 
-print(as_espadas
+print(as_espadas)
 print(as_espadas.naipe
 print(as_espadas.valor
