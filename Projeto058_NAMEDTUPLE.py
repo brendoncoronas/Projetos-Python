d5936c5d0d0875ed8ed6ef4e79b44561096d0621
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-26 09:29:16 -0300

    Add files via upload

---
+++ Projeto058_NAMEDTUPLE.py
@@ -0,0 +1,11 @@
+
+from typing import NamedTuple
+from collections import namedtuple
+
+Carta = namedtuple('carta', ['valor', 'naipe # type: ignore
+
+as_espadas = Carta('A', 'espada
+
+print(as_espadas
+print(as_espadas.naipe
+print(as_espadas.valor
