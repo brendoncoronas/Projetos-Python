33c8140be6da16231b7bf4ff523444847eec7a29
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-29 19:51:12 -0300

    Update Projeto061_DATACLASS_configurações.py

--- Projeto061_DATACLASS_configurações.py
+++ Projeto061_DATACLASS_configurações.py
@@ -7,7 +7,7 @@ class Pessoa:
     sobrenome: str
 
 
-if __name__ = _main__
+if __name__ = __main__
     p1 = Pessoa'uiz, otavio
     printasdictp1  # podemos manipular normalmente como um dicionario ou
     printastuplep1  # uma tupla
