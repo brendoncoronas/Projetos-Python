8a2314874e346b88a9c82aa38b3fb4579c006dc4
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-29 19:48:05 -0300

    Add files via upload

---
+++ Projeto061_DATACLASS_configurações.py
@@ -0,0 +1,13 @@
+from dataclasses import asdict, astuple, dataclass
+
+
+dataclass
+class Pessoa
+    nome str
+    sobrenome str
+
+
+if name = main
+    p1 = Pessoa'uiz, otavio
+    printasdictp1  # podemos manipular normalmente como um dicionario ou
+    printastuplep1  # uma tupla
