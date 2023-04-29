0517ae2510b33ee12b4303b6721cb45044ca3139
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-04-29 19:53:38 -0300

    Update Projeto061_DATACLASS_configurações.py

--- Projeto061_DATACLASS_configurações.py
+++ Projeto061_DATACLASS_configurações.py
@@ -10,4 +10,4 @@ class Pessoa:
 if __name__ = __main__
     p1 = Pessoa('luiz', 'otavio')
     print(asdictp1)  # podemos manipular normalmente como um dicionario ou
-    printastuplep1  # uma tupla
+    print(astuplep1  # uma tupla
