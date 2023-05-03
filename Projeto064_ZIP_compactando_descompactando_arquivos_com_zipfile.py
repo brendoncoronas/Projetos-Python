8075f7417cdfab88f1ccb08e94b4f5aa8de20a36
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-03 09:14:03 -0300

    Update Projeto064_ZIP_compactando_descompactando_arquivos_com_zipfile.py

--- Projeto064_ZIP_compactando_descompactando_arquivos_com_zipfile.py
+++ Projeto064_ZIP_compactando_descompactando_arquivos_com_zipfile.py
@@ -8,7 +8,7 @@ from zipfile import ZipFile
 # Caminhos
 CAMINHO_RAIZ = Path(__file__).parent
 CAMINHO_ZIP_DIR = CAMINHO_RAIZ / 'aula_186_diretorio_zip'
-CAMINHO_COMPACTADO = CAMINHO_RAIZ / aula186_compactado.zip
+CAMINHO_COMPACTADO = CAMINHO_RAIZ / aula186_compactado.zip'
 CAMINHO_DESCOMPACTADO = CAMINHO_RAIZ / aula186_descompactado
 
 shutil.rmtree(CAMINHO_ZIP_DIR, ignore_errors=True
