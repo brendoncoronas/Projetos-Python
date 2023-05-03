f2f4f680850a3b760df478ebc167dd79635d221e
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-02 21:22:59 -0300

    Add files via upload

---
+++ Projeto063_CALENDARIO.py
@@ -0,0 +1,15 @@
+
+import calendar
+
+print(calendar.calendar(2023
+print(calendar.month(2023, 12
+
+print(calendar.monthrange(2022, 12
+print(list(enumerate(calendar.day_name
+
+for week in calendar.monthcalendar(2023, 12
+    for day in week
+        if day == 0  # os dias q n sao do mes q qremos ficam '0', ate começar
+            # o mes desejado
+            continue
+        print(day
