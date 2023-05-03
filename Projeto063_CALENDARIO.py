65e82d32b4c608e03be177c83867d63a28951fb1
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-02 21:25:17 -0300

    Update Projeto063_CALENDARIO.py

--- Projeto063_CALENDARIO.py
+++ Projeto063_CALENDARIO.py
@@ -8,7 +8,7 @@ print(calendar.monthrange(2022, 12))
 print(list(enumerate(calendar.day_name)))
 
 for week in calendar.monthcalendar(2023, 12):
-    for day in week
+    for day in week:
         if day == 0  # os dias q n sao do mes q qremos ficam '0', ate começar
             # o mes desejado
             continue
