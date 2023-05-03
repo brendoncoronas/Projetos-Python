16634965bbe7cc079a6b7878dcc7349f446b0cbf
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-02 21:23:55 -0300

    Update Projeto063_CALENDARIO.py

--- Projeto063_CALENDARIO.py
+++ Projeto063_CALENDARIO.py
@@ -2,7 +2,7 @@
 import calendar
 
 print(calendar.calendar(2023))
-print(calendar.month(2023, 12
+print(calendar.month(2023, 12)
 
 print(calendar.monthrange(2022, 12
 print(list(enumerate(calendar.day_name
