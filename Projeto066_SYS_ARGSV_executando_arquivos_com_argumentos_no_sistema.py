42efaf38aa6827758f0478d360ff3b6c09c894be
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-05 20:11:22 -0300

    Update Projeto066_SYS_ARGSV_executando_arquivos_com_argumentos_no_sistema.py

--- Projeto066_SYS_ARGSV_executando_arquivos_com_argumentos_no_sistema.py
+++ Projeto066_SYS_ARGSV_executando_arquivos_com_argumentos_no_sistema.py
@@ -4,7 +4,7 @@ argumentos = sys.argv
 qtd_argumentos = len(argumentos)
 
 if qtd_argumentos <= 1:
-    print('voce nao passou argumentos'
+    print('voce nao passou argumentos')
 else:
     try:
         print(f'voce passou o argumento {argumentos[0
