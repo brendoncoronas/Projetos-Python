4329b0d4f620bd79d37b47138f9313c0f9899b1a
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-05 20:07:57 -0300

    Add files via upload

---
+++ Projeto066_SYS_ARGSV_executando_arquivos_com_argumentos_no_sistema.py
@@ -0,0 +1,14 @@
+
+
+argumentos = sys.argv
+qtd_argumentos = len(argumentos
+
+if qtd_argumentos <= 1:
+    print('voce nao passou argumentos
+else:
+    try:
+        print(f'voce passou o argumento {argumentos[0
+
+    except IndexError
+        print('faltam argumentos
+
