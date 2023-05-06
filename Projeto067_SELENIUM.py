a6008253d4e5772acd5ecd376dc0cd6854715047
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-06 20:16:06 -0300

    Update Projeto067_SELENIUM.py

--- Projeto067_SELENIUM.py
+++ Projeto067_SELENIUM.py
@@ -1,4 +1,4 @@
-# faça um projeto que most
+# faça um projeto que mostr
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
