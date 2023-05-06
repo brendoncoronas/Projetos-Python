cb98ffbcbd7536b1a4d27ace80955957269103c6
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-06 20:16:53 -0300

    Update Projeto067_SELENIUM.py

--- Projeto067_SELENIUM.py
+++ Projeto067_SELENIUM.py
@@ -1,4 +1,4 @@
-# faça um projeto que mostre a usab
+# faça um projeto que mostre a usabi
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
