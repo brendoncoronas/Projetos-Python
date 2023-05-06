afd1667aad9dcce728625e873258a085cf67088e
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-06 20:07:24 -0300

    Add files via upload

---
+++ Projeto067_SELENIUM.py
@@ -0,0 +1,57 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
+
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(_file_).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if _name_ == '_main_':
+    TIME_TO_WAIT = 10
+
+    # Example
+    # options = '--headless', '--disable-gpu',
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    # Como antes
+    browser.get('https://www.google.com')
+
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+    search_input.send_keys('Hello World!')
+    search_input.send_keys(Keys.ENTER)
+
+    # Dorme por 10 segundos
+    sleep(TIME_TO_WAIT)
