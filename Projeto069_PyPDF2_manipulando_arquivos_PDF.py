b4aa0fde78df6b2a8f8ec10c0081373de46b9be7
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-08 22:04:02 -0300

    Update Projeto069_PyPDF2_manipulando_arquivos_PDF.py

--- Projeto069_PyPDF2_manipulando_arquivos_PDF.py
+++ Projeto069_PyPDF2_manipulando_arquivos_PDF.py
@@ -18,7 +18,7 @@ for page in reader.pages:
     print()
 
 page0 = reader.pages[0]
-imagem0 = page0.images[0
+imagem0 = page0.images[0]
 
 # print(page0.extract_text())
 with open(PASTA_NOVA / imagem0.name, 'wb as fp
