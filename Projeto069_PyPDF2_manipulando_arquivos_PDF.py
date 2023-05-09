983b5b5f58a41095403ca86a59de43688a96958a
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-08 22:01:24 -0300

    Update Projeto069_PyPDF2_manipulando_arquivos_PDF.py

--- Projeto069_PyPDF2_manipulando_arquivos_PDF.py
+++ Projeto069_PyPDF2_manipulando_arquivos_PDF.py
@@ -4,7 +4,7 @@ from PyPDF2 import PdfReader
 
 PASTA_RAIZ = Path(__file__).parent
 PASTA_ORIGINAIS = PASTA_RAIZ / 'aula185_PDF'
-PASTA_NOVA = PASTA_RAIZ / 'arquivos_novos
+PASTA_NOVA = PASTA_RAIZ / 'arquivos_novos'
 
 RELATORIO_BACEN = PASTA_ORIGINAIS / 'R20220527.pdf
 
