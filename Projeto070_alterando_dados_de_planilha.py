2e22246257e815b54a2b918d6e769967f061b925
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-09 19:25:11 -0300

    Update Projeto070_alterando_dados_de_planilha.py

--- Projeto070_alterando_dados_de_planilha.py
+++ Projeto070_alterando_dados_de_planilha.py
@@ -19,7 +19,7 @@ worksheet: Worksheet = workbook[sheet_name]
 row: tuple[Cell]
 for row in worksheet.iter_rows(min_row=2):
     for cell in row:
-        print(cell.value, end='\t'
+        print(cell.value, end='\t')
 
         if cell.value == 'Maria
             worksheet.cell(cell.row, 2, 23
