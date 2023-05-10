4f1d1d9ff06af6298716dea915462196a526bfdb
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-10 19:49:26 -0300

    Update Projeto071_PILLOW.py

--- Projeto071_PILLOW.py
+++ Projeto071_PILLOW.py
@@ -15,7 +15,7 @@ exif = pil_image.info['exif']
 new_width = 640
 new_height = round(height * new_width / width)
 
-new_image = pil_image.resize(size=(new_width, new_height
+new_image = pil_image.resize(size=(new_width, new_height)
 new_image.save(
     NEW_IMAGE,
     optimize=True,
