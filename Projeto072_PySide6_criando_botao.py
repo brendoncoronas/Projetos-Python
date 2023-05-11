79547b598ea05758400b159499d6ebc9490ca173
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-11 19:59:11 -0300

    Update Projeto072_PySide6_criando_botao.py

--- Projeto072_PySide6_criando_botao.py
+++ Projeto072_PySide6_criando_botao.py
@@ -1,4 +1,4 @@
-# faça um projeto com PySide6 mostrando um pouco da
+# faça um projeto com PySide6 mostrando um pouco da usabilidade
 import sys
 
 # QApplication - é responsavel por gerenciar a nossa aplicação e tbm
