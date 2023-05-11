804fd6f0f6169567aaf7675d0fb0f321526cbdfe
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-11 19:50:12 -0300

    Add files via upload

---
+++ Projeto072_PySide6_criando_botao.py
@@ -0,0 +1,28 @@
+
+import sys
+
+# QApplication - é responsavel por gerenciar a nossa aplicação e tbm
+# e tbm nao exibe nada na tela
+from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
+
+app = QApplication(sys.argv  # type: ignore
+
+botao = QPushButton('texto do botao
+botao.setStyleSheet('font-size: 20px;
+botao.show()  # adiciona o widget na hierarquia e exibe a janela
+
+botao2 = QPushButton('botao 2
+botao2.setStyleSheet('font-size: 20px;
+
+
+central_widget = QWidget(
+layout = QVBoxLayout(
+central_widget.setLayout(layout
+
+layout.addWidget(botao
+
+central_widget.show(  # central_widget entre na hierarquia e mostre sua janela
+
+# o loop da aplicação:
+
+app.exec(
