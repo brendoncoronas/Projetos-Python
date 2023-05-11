39d73381c387c886849317383699c1a97760f5bd
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-11 19:51:41 -0300

    Update Projeto072_PySide6_criando_botao.py

--- Projeto072_PySide6_criando_botao.py
+++ Projeto072_PySide6_criando_botao.py
@@ -8,7 +8,7 @@ from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 app = QApplication(sys.argv)  # type: ignore
 
 botao = QPushButton('texto do botao')
-botao.setStyleSheet('font-size: 20px;'
+botao.setStyleSheet('font-size: 20px;')
 botao.show()  # adiciona o widget na hierarquia e exibe a janela
 
 botao2 = QPushButton('botao 2
