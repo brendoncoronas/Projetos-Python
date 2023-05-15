07421e8c3a5c92797eab1c6772fec9a068be8c25
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-15 19:43:01 -0300

    Update Projeto074_classes_e_herança_com_PySide6.py

--- Projeto074_classes_e_herança_com_PySide6.py
+++ Projeto074_classes_e_herança_com_PySide6.py
@@ -26,7 +26,7 @@ class MyWindow(QMainWindow):
         self.central_widget.setLayout(self.grid_layout)
 
         self.grid_layout.addWidget(self.botao1, 1, 1, 1, 1)
-        self.grid_layout.addWidget(self.botao2, 1, 2, 1, 1
+        self.grid_layout.addWidget(self.botao2, 1, 2, 1, 1)
         self.grid_layout.addWidget(self.botao3, 3, 1, 1, 2
 
         # statusBar
