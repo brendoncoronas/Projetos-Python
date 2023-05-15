daeb625080bd17df94e2b58ce9c33e91bf0d8f3b
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-15 19:44:51 -0300

    Update Projeto074_classes_e_herança_com_PySide6.py

--- Projeto074_classes_e_herança_com_PySide6.py
+++ Projeto074_classes_e_herança_com_PySide6.py
@@ -37,7 +37,7 @@ class MyWindow(QMainWindow):
         self.menu = self.menuBar()
         self.primeiro_menu = self.menu.addMenu('Primeiro menu')
         self.primeira_acao = self.primeiro_menu.addAction('Primeira ação')
-        self.primeira_acao.triggered.connect(  # type:ignore
+        self.primeira_acao.triggered.connect()  # type:ignore
             self.muda_mensagem_da_status_bar
 
         self.segunda_action = self.primeiro_menu.addAction('Segunda ação
