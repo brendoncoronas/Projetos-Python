9a1bed474537c79f190a253da7bf0c3faa4fefd2
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-24 20:59:05 -0300

    Update Projeto075_JANELAS_e_FUNCOES_com_PySide6.py

--- Projeto075_JANELAS_e_FUNCOES_com_PySide6.py
+++ Projeto075_JANELAS_e_FUNCOES_com_PySide6.py
@@ -78,6 +78,6 @@ class MyWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MyWindow(  # estamos usando agora o QMainWindow pois tem mais
+    window = MyWindow()  # estamos usando agora o QMainWindow pois tem mais
     window.show(
     app.exec(    # executa o loop da aplicação
