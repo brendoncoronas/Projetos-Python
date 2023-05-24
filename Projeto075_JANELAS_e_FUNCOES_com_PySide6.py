41dd6ff43401a594ff9ed6dd56af83544d859ca5
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-24 20:48:22 -0300

    Update Projeto075_JANELAS_e_FUNCOES_com_PySide6.py

--- Projeto075_JANELAS_e_FUNCOES_com_PySide6.py
+++ Projeto075_JANELAS_e_FUNCOES_com_PySide6.py
@@ -9,7 +9,7 @@ from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow,
 
 
 class MyWindow(QMainWindow):
-    def __init__(self, parent=None)
+    def __init__(self, parent=None):
         super().__init__(parent
 
         # botao1
