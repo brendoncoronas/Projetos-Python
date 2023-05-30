817afe905bf2b067bbc622e0dd7da9ea84563ee9
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-05-30 19:48:14 -0300

    Update Projeto077_PESSOA.py

--- Projeto077_PESSOA.py
+++ Projeto077_PESSOA.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     c1.conta = aula147_CONTA.ContaCorrente(111, 222, 0, 0)
     print(c1)
     print(c1.conta)
-    c2 = Cliente('brendon', 23
+    c2 = Cliente('brendon', 23)
     c2.conta = aula147_CONTA.ContaCorrente(112, 223, 100, 100
     print(c2
     print(c2.conta
