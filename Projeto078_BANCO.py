214fc896b82b9c8cd19972f2703fe07e38ad6c26
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-01 21:15:30 -0300

    Update Projeto078_BANCO.py

--- Projeto078_BANCO.py
+++ Projeto078_BANCO.py
@@ -72,6 +72,6 @@ if __name__ == '__main__':
     banco.agencias.extend([111, 222])
 
     if banco.autenticar(c1, cc1):
-        cc1.depositar(10
+        cc1.depositar(10)
         c1.conta.depositar(100
         print(c1.conta
