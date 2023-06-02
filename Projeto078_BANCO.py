791e1ceaa6b20c9e64acd5c1d578335b029c355c
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-01 21:12:46 -0300

    Update Projeto078_BANCO.py

--- Projeto078_BANCO.py
+++ Projeto078_BANCO.py
@@ -59,7 +59,7 @@ class Banco:
 
 
 if __name__ == '__main__':
-    c1 = aula148_PESSOA.Cliente('luiz', 30
+    c1 = aula148_PESSOA.Cliente('luiz', 30)
     cc1 = aula147_CONTA.ContaCorrente(111, 222, 0, 0
     c1.conta = cc1
 
