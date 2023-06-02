73f7be08112f2242d4253c85f2a15560bcaa6c5d
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-01 21:13:32 -0300

    Update Projeto078_BANCO.py

--- Projeto078_BANCO.py
+++ Projeto078_BANCO.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     c2 = aula148_PESSOA.Cliente('brendon', 23)
     cp1 = aula147_CONTA.ContaPoupanca(112, 223, 100)
     c2.conta = cp1
-    banco = Banco(
+    banco = Banco()
     banco.clientes.extend([c1, c2
     banco.contas.extend([cc1, cp1
     banco.agencias.extend([111, 222
