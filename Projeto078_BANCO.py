ecce51e4c8b3324f3cd00fff1b3cb03c96c6cd3f
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-01 21:14:20 -0300

    Update Projeto078_BANCO.py

--- Projeto078_BANCO.py
+++ Projeto078_BANCO.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     c2.conta = cp1
     banco = Banco()
     banco.clientes.extend([c1, c2])
-    banco.contas.extend([cc1, cp1
+    banco.contas.extend([cc1, cp1]
     banco.agencias.extend([111, 222
 
     if banco.autenticar(c1, cc1
