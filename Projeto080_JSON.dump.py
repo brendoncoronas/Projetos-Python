b0c79d1c693d94ce0c2a038e360d93835aec17da
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-03 20:39:00 -0300

    Update Projeto080_JSON.dump.py

--- Projeto080_JSON.dump.py
+++ Projeto080_JSON.dump.py
@@ -15,7 +15,7 @@ filme = {
     'title': 'O Senhor dos Anéis: A Sociedade do Anel',
     'original_title': 'The Lord of the Rings: The Fellowship of the Ring',
 
-    'is_movie': True
+    'is_movie': True,
     'imdb_rating': 8.8
     'year': 2001
     'characters': ['Frodo', 'Sam', 'Gandalf', 'Legolas', 'Boromir'
