992b0678142c990975d6e547f31ce5e01befb99c
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-02 22:00:05 -0300

    Update Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py

--- Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py
+++ Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py
@@ -6,7 +6,7 @@ DESKTOP = os.path.join(HOME, 'Desktop')
 PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
 NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
 
-os.makedirs(NOVA_PASTA, exist_ok=True
+os.makedirs(NOVA_PASTA, exist_ok=True)
 
 for root, dirs, files in os.walk(PASTA_ORIGINAL
     for dir_ in dirs
