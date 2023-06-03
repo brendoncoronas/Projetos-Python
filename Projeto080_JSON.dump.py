5eb6480b46e58bb327f01c560bfe8e60364b27de
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-03 20:38:21 -0300

    Update Projeto080_JSON.dump.py

--- Projeto080_JSON.dump.py
+++ Projeto080_JSON.dump.py
@@ -13,7 +13,7 @@ CAMINHO_ABSOLUTO_ARQUIVO = os.path.abspath(
 
 filme = {
     'title': 'O Senhor dos Anéis: A Sociedade do Anel',
-    'original_title': 'The Lord of the Rings: The Fellowship of the Ring
+    'original_title': 'The Lord of the Rings: The Fellowship of the Ring'
 
     'is_movie': True
     'imdb_rating': 8.8
