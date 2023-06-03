0ea422a6af685bca3f747b0e694a84cb2af614e4
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-02 22:02:39 -0300

    Update Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py

--- Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py
+++ Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py
@@ -19,5 +19,6 @@ for root, dirs, files in os.walk(PASTA_ORIGINAL):
         caminho_arquivo = os.path.join(root, file)
         caminnho_novo_arquivo = os.path.join()
             root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+            )
         
         shutil.copy(caminho_arquivo, caminnho_novo_arquivo
