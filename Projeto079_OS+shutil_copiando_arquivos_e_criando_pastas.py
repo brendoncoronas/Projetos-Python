265c013f99cbe524d9e0a77e337461a0c19466bd
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-02 22:00:49 -0300

    Update Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py

--- Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py
+++ Projeto079_OS+shutil_copiando_arquivos_e_criando_pastas.py
@@ -9,7 +9,7 @@ NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
 os.makedirs(NOVA_PASTA, exist_ok=True)
 
 for root, dirs, files in os.walk(PASTA_ORIGINAL):
-    for dir_ in dirs
+    for dir_ in dirs:
         caminnho_novo_diretorio = os.path.join(
             root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
         )
