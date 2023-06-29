6e79996d77d4815ad6625533b045010ce4bd877c
brendoncoronas <123513410+brendoncoronas@users.noreply.github.com>
2023-06-29 20:01:56 -0300

    Update Projeto004_verificação_DeNome.py

--- Projeto004_verificação_DeNome.py
+++ Projeto004_verificação_DeNome.py
@@ -13,7 +13,7 @@ tamanho_nome = len(nome)
 if tamanho_nome>= 1:
     if tamanho_nome <= 4:
         print('seu nome é curto.')
-    elif tamanho_nome >= 5 and tamanho_nome <=6:
+    elif tamanho_nome >= 5 and tamanho_nome <=6
         print('seu nome é normal.')
     else:
         print('seu nome é muito grande.')        
